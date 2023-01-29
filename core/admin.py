294533b533706eb5697534f74b013910a75943fd
coding-instinct <72700292+coding-instinct@users.noreply.github.com>
2023-01-29 16:17:24 +0530

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import NFT
-# Register your models here.
-@admin.register(NFT)
-class NftModel(admin.ModelAdmin):
-    list_display=['tokenid','photo_main']
